323c4100c9ccf2ece07d3f837b04432afb09b10f
unknown <k2wackie@gmail.com>
2022-03-27 17:15:11 +0900

    소스파일 src폴더로 이동

---
+++ app/app.js
@@ -0,0 +1,16 @@
+"use strict";
+//module
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+//routing
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.use("/", home); // use => 미들웨어를 등록해주는 메서드
+
+module.exports = app;
